e2f7749ab8521093fcb1bb8292522de9ce98bab4
daignaultz <daignaultz@gmail.com>
2020-03-17 11:31:54 -0400

    added letter grade

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -1,4 +1,5 @@
-﻿using GradeBook.Enums;
+﻿using System;
+using GradeBook.Enums;
 
 namespace GradeBook.GradeBooks
 {
@@ -7,6 +8,28 @@ namespace GradeBook.GradeBooks
         public RankedGradeBook(string name) : base(name)
         {
             Type = GradeBookType.Ranked;
+        }
+
+        public override char GetLetterGrade(double averageGrade)
+        {
+            if (Students.Count < 5)
+            {
+                throw new InvalidOperationException("Ranked-grading requires a minimum of 5 students to work");
+            }
+
+            switch (averageGrade)
+            {
+                case double n when n >= 80:
+                    return 'A';
+                case double n when n >= 60:
+                    return 'A';
+                case double n when n >= 40:
+                    return 'A';
+                case double n when n >= 20:
+                    return 'A';
+                default:
+                    return 'F';
+            }
         }
     }
 }
